8c192cda132c77a353d3660c3a04be7d6f80fa5c
Rob Scott <rob@scottclan.cc>
2014-10-06 11:46:13 -0400

    Build Param tests

--- Deployer.Tests/Deployer.Services.Tests/Config/ConfigTests.cs
+++ Deployer.Tests/Deployer.Services.Tests/Config/ConfigTests.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using Deployer.Services.Builders;
-using Deployer.Services.Config;
-using Deployer.Services.Models;
+﻿using Deployer.Services.Config;
 using Moq;
 using NUnit.Framework;
+using System.Collections;
 
 namespace Deployer.Tests.Config
 {
@@ -20,15 +18,61 @@ namespace Deployer.Tests.Config
 			_jsonPersist = new Mock<IJsonPersistence>();
 			_slugCreator = new Mock<ISlugCreator>();
 
-			_jsonPersist.Setup(x => x.Read(@"\root\config\projects.json")).Returns(new Hashtable());
-
 			_sut = new RealConfigurationService(@"\root\", _jsonPersist.Object, _slugCreator.Object);
 		}
 
 		[Test]
-		public void Start_with_zero_projects()
+		public void Empty_build_params()
 		{
-			//_sut.GetBuildParams();
+			_jsonPersist.Setup(x => x.Read(@"\root\config\slug-1.json")).Returns(new Hashtable());
+
+			var hash = _sut.GetBuildParams("slug-1");
+
+			Assert.IsNotNull(hash);
+			Assert.AreEqual(0, hash.Count);
+			_jsonPersist.Verify(x => x.Read(It.IsAny<string>()), Times.Once);
+			_jsonPersist.Verify(x => x.Read(@"\root\config\slug-1.json"), Times.Once);
+		}
+
+		[Test]
+		public void Read_params()
+		{
+			var mockHash = new Hashtable
+				{
+					{"key1", "value1"},
+					{"key2", "value2"}
+				};
+			_jsonPersist.Setup(x => x.Read(@"\root\config\slug-1.json")).Returns(mockHash);
+
+			var actualHash = _sut.GetBuildParams("slug-1");
+
+			Assert.IsNotNull(actualHash);
+			Assert.AreEqual(2, actualHash.Count);
+			Assert.AreEqual("value1", actualHash["key1"]);
+			Assert.AreEqual("value2", actualHash["key2"]);
+			_jsonPersist.Verify(x => x.Read(It.IsAny<string>()), Times.Once);
+			_jsonPersist.Verify(x => x.Read(@"\root\config\slug-1.json"), Times.Once);
+		}
+
+		[Test]
+		public void Write_params()
+		{
+			var mockHash = new Hashtable
+				{
+					{"key1", "value1"},
+					{"key2", "value2"}
+				};
+			_jsonPersist.Setup(x => x.Write(@"\root\config\slug-1.json", It.IsAny<Hashtable>()))
+			            .Callback((string path, Hashtable hash) =>
+				            {
+					            Assert.AreEqual(2, hash.Count);
+					            Assert.AreEqual("value1", hash["key1"]);
+					            Assert.AreEqual("value2", hash["key2"]);
+				            });
+
+			_sut.SaveBuildParams("slug-1", mockHash);
+
+			_jsonPersist.Verify(x => x.Write(@"\root\config\slug-1.json", It.IsAny<Hashtable>()), Times.Once);
 		}
 	}
 }
