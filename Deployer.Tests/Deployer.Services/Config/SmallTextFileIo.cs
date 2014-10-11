086d2ac30d941a28c2cf63af5ab9b3d5fac7160f
Rob Scott <rob@scottclan.cc>
2014-10-11 10:31:40 -0400

    I/O has been flaky. Often a write will result in a zero-byte file and an exception. This tends to wipe out existing data. Therefore when writing smaller files, write the data to a temp file and delete/rename.

--- Deployer.Tests/Deployer.Services/Config/SmallTextFileIo.cs
+++ Deployer.Tests/Deployer.Services/Config/SmallTextFileIo.cs
@@ -23,7 +23,24 @@ namespace Deployer.Services.Config
 		public void Write(string filePath, string content)
 		{
 			var bytes = Encoding.UTF8.GetBytes(content);
-			File.WriteAllBytes(filePath, bytes);
+
+			// Write to a temporary file first
+			var tempPath = GetTempPath(filePath);
+			File.WriteAllBytes(tempPath, bytes);
+
+			// If it succeeds, delete the old one
+			File.Delete(filePath);
+
+			// ... and rename the temp file
+			File.Move(tempPath, filePath);
+		}
+
+		private static string GetTempPath(string filePath)
+		{
+			var tempPath = filePath + ".temp";
+			if(File.Exists(tempPath))
+				File.Delete(tempPath);
+			return tempPath;
 		}
 	}
 }
