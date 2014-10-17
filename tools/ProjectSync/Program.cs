559de9057ff8777776eb08ad6d6fdb50dad9fda0
Rob Scott <rob@deployerofcodes.com>
2014-10-16 20:36:45 -0400

    ProjectSync updates

--- tools/ProjectSync/Program.cs
+++ tools/ProjectSync/Program.cs
@@ -1,42 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Reflection;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.XPath;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace ProjectSync
 {
-	internal class Program
-	{
-		private static void Main(string[] args)
-		{
-			var startingDir = Assembly.GetExecutingAssembly().Location;
-			var rootDir = GetRootProjectDirectory(startingDir);
+    internal class Program
+    {
+        private static void Main()
+        {
+            var startingDir = Assembly.GetExecutingAssembly().Location;
+            var rootDir = GetRootProjectDirectory(startingDir);
+            var ctrl = new ProjectController(rootDir, @"Deployer.Tests");
 
-			var pathSource = Path.Combine(rootDir, @"Deployer.Tests\Deployer.Services\Deployer.Services.csproj");
-			var pathTarget = Path.Combine(rootDir, @"Deployer.Services\Deployer.Services.csproj");
+            ctrl.Sync(@"Deployer.Services\Deployer.Services.csproj", @"..\Deployer.Tests\Deployer.Services\");
+            ctrl.Sync(@"NeonMika\NeonMika.csproj", @"..\Deployer.Tests\NeonMika\");
+        }
 
-			var th = new Synchronizer();
-			th.DoStuff(pathSource, pathTarget);
-		}
-
-		private static string GetRootProjectDirectory(string path)
-		{
-			var dir = Path.GetDirectoryName(path);
-			var currentDir = new DirectoryInfo(dir);
-			while (true)
-			{
-				var testsDir = Path.Combine(currentDir.FullName, "Deployer.Tests");
-				if (Directory.Exists(testsDir))
-					return currentDir.FullName;
-				currentDir = currentDir.Parent;
-			}
-		}
-	}
+        private static string GetRootProjectDirectory(string path)
+        {
+            var dir = Path.GetDirectoryName(path);
+            if (string.IsNullOrWhiteSpace(dir)) return string.Empty;
+            var currentDir = new DirectoryInfo(dir);
+            while (true)
+            {
+                var testsDir = Path.Combine(currentDir.FullName, "Deployer.Tests");
+                if (Directory.Exists(testsDir))
+                    return currentDir.FullName;
+                currentDir = currentDir.Parent;
+                if (currentDir == null) return string.Empty;
+            }
+        }
+    }
 }
